0116bdb8fa2ea9ddacff000452be74f4804c87a8
hdknr <gmail@hdknr.com>
2012-07-22 15:36:19 +0900

    celeryd -B/--beat option doesn't seem to work

--- sample/app/run.py
+++ sample/app/run.py
@@ -9,6 +9,9 @@ PID_FILE="/Users/hide/Desktop/tomate.pid"  #: celery worker PID file
 NODE="celery"           #: celery = default node
 LOG_LEVEL="DEBUG"       #: celery log level
 
+# Unfortunately, -B/--beat options doesn't seem to work
+# - http://docs.celeryproject.org/en/latest/faq.html#the-b-beat-option-to-celeryd-doesn-t-work 
+#
 def configure(args):
     ''' return django-celery parameter for specified args
 
@@ -20,9 +23,10 @@ def configure(args):
     if  len(args) < 3 or args[2] == "start" :
         #: start worker
         return [ args[0], "celeryd",
-                "--loglevel" , LOG_LEVEL,
-                "--pidfile" , PID_FILE, 
-                "--logfile" , LOG_FILE 
+                "--loglevel=%s" % LOG_LEVEL,
+                "--pidfile=%s" % PID_FILE, 
+                "--logfile=%s" % LOG_FILE ,
+                "--scheduler=djcelery.schedulers.DatabaseScheduler",
             ]   
 
     if  len(args) >2 and args[2] == "stop":
