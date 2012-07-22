9d2f5ebb92559d7da040d94e1b270caf07fb3453
hdknr <gmail@hdknr.com>
2012-07-22 14:16:19 +0900

    bash utility for convenience

---
+++ sample/env.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TOMATO_START()
+{
+    tomato.py  /Users/hide/ve/docs/src/tomate/sample/app start &
+}
+
+TOMATO_STOP()
+{
+    tomato.py  /Users/hide/ve/docs/src/tomate/sample/app stop
+}
